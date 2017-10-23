243225c8179c3bc71d5cd6434f95cf02736b0986
TMateo <33035891+TMateo@users.noreply.github.com>
2017-10-23 13:45:03 -0400

    Question #5
    added string name and user input propmt with output

--- hello.java
+++ hello.java
@@ -3,13 +3,14 @@
 public class hello {
 
     public static void main(String [] args) {
+        String name;
+        Scanner scanner1 : new Scanner(System.in);
+        System.out.printIn.("Your Name: ");
+        name = scanner1.nextLine();
 
+        System.out.println("Hello" + name + "my name is Tristan");
 
-	// added  line	
-
-        System.out.println("Hello Class!");
-
-
+       
 
     }
 }
